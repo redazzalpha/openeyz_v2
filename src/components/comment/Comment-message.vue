c05d401b979c9928f1da0597a7e9a2c09d137500
kyzer <redazzkillah@gmail.com>
2022-05-27 20:31:04 +0200

    update add comment

--- src/components/comment/Comment-message.vue
+++ src/components/comment/Comment-message.vue
@@ -7,6 +7,7 @@
                 <Avatar :user="currentUser" size="40" />
                 <v-textarea v-model="comment" placeholder="leave comment..." auto-grow outlined rows="1" row-height="15"
                     hide-details>
+                    <!-- FIXME: fix btn bug sometimes does not appear - try to change with append directive -->
                     <!-- send-comment-button -->
                     <template v-slot:append>
                         <v-btn icon color="primary" @click="send">
@@ -21,13 +22,27 @@
             <v-col>
                 <v-card elevation="0">
                     <!-- comment-title -->
-                    <v-card-title primary-title class="text-decoration-underline">
+                    <v-card-title primary-title class="text-decoration-underline text-body-2 text-sm-body-1">
                         Comment:
                     </v-card-title>
-                    <v-card-text v-for="(item, index) in comments" :key="index">
-                    {{item.comment.content}}
-                    </v-card-text>
-                    <!-- comment-content -->
+                    <!-- comment-content-card -->
+                    <v-card v-for="(item, index) in comments" :key="index" class="d-flex align-center mb-2"
+                        elevation="0">
+                        <Avatar :user="item.comment.author" size="40" class="flex-shrink-1" />
+                        <div class="message-arrowed flex-grow-1 elevation-3" style="background-color:#2196F3; position: relative;">
+                            <v-card-title primary-title class="text-body-2 text-sm-subtitle-1 white--text pa-2 mb-2">
+                                {{ item.comment.author.name }}
+                                said on
+                                {{ item.creation }}
+                            </v-card-title>
+                            <div class="message-arrowed"></div>
+                            <v-card-text class="white--text">
+                                <div class="d-flex">
+                                    {{ item.comment.content }}
+                                </div>
+                            </v-card-text>
+                        </div>
+                    </v-card>
                 </v-card>
             </v-col>
         </v-row>
@@ -36,8 +51,8 @@
 
 <script lang="ts">
 import Vue from 'vue';
-import Avatar from '../cpn/Appbar-cpn.vue';
-import { mapState, mapActions} from 'vuex';
+import Avatar from '../cpn/Avatar-cpn.vue';
+import { mapState, mapActions } from 'vuex';
 import { httpRequest } from '@/utils/http';
 import * as Defines from '@/utils/defines';
 export default Vue.extend({
@@ -78,5 +93,15 @@ export default Vue.extend({
 <style lang="scss">
 .v-input__append-inner {
     margin-top: 8px !important;
+}
+
+.message-arrowed::after {
+    content: "";
+    position: absolute;
+    left: -26px;
+    top: 28%;
+    border: solid transparent 15px;
+    border-left: solid transparent 15px;
+    border-right: solid #2196F3 15px;
 }
 </style>
